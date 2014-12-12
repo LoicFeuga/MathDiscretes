eff1b1b6f4786490bc7651fc3b871e0dbc35f69e
Loïc <loic.feuga@gmail.com>
2014-12-12 13:18:01 +0100

    Première méthode enumBP(int n) NON RECURSIVE ! c'est possible qu'il manque des possibilités à vérifier

--- model/MotBienParenthese.java
+++ model/MotBienParenthese.java
@@ -1,46 +1,87 @@
 package model;
 
+import java.util.ArrayList;
+
 public class MotBienParenthese {
 	private static String parentheseOuvrante = "(";
 	private static String parentheseFermante = ")";
 	private static int compteurOuvrante = 0;
 	private static int compteurFermante = 0;
+	private static ArrayList list = new ArrayList();
+	private static int compteur = 0;
 	private static int altitude = 0;
 	private static int nb = 0;
 
-	public static void enumMotsBP(int n){		
+	public static void enumMotsBP(int n){	
 		n*=2;
+
 		if(n <= 0){ 
-			System.out.println("0 mot");return;
+			System.out.println("0 mot");
+			return;
 		}
-		if(n == 2){ 
-			System.out.println("()"); return;
-		}
-
-		if(altitude == 0){
+		else if(n == 2){ 
 			addOuvrante();
-		}
-		else if(altitude >= n/2){
 			addFermante();
+			System.out.println("");
+			return;
 		}
+		else {
+			list.add("");
+			for(int i = 0 ; i < list.size() ; i++){
+				for(int j = 0; j < list.size() ; j++){
+					String a = apliquerConstructeur((String)list.get(i),(String) list.get(j));
+					String b = apliquerConstructeur((String)list.get(j),(String) list.get(i));
+					
+					if(a.length() <= n){
+						list.add(a);
 
-		if(altitude > nb){
+					}else if (b.length() <= n){
+						list.add(b);
+					}
+				}
+			}
+
+			delete(n);
 			
 		}
 
-		nb++;
-		enumMotsBP(n);
+	}
+
+	private static void delete(int n){
+		for(int j = 0; j < list.size(); j++){
+		for(int i = 0 ; i < list.size(); i++){
+			if(((String) list.get(i)).length() != n ){
+				list.remove(i);
+			}
+		}
+		
+		}
+		for(int i = 0; i < list.size();i++){
+			System.out.println(list.get(i));
+		}
 	}
 	
+	private static String apliquerConstructeur(String a, String b){
+		return parentheseOuvrante + a + parentheseFermante +b;		
+	}
+
 	private static void addOuvrante(){
 		System.out.print("(");
 		compteurOuvrante++;
+		compteur++;
 		altitude++;
 	}
-	
+
+	public static void reset(){
+		nb = 0;
+		compteur = 0;
+		compteurFermante = 0;
+		compteurOuvrante = 0;
+	}
 	private static void addFermante(){
-		System.out.println(")");
+		System.out.print(")");
 		compteurFermante++;
+		compteur++;
 		altitude--;
 	}
 
--- tests/TestMotBienParenthese.java
+++ tests/TestMotBienParenthese.java
@@ -3,10 +3,19 @@ package tests;
 import model.MotBienParenthese;
 
 public class TestMotBienParenthese {
-	
+
 	public static void main(String[] args) {
-		MotBienParenthese m = new MotBienParenthese();
-		
-		MotBienParenthese.enumMotsBP(6);
+		System.out.println("Les mots bien parenthésé de longueur 0 :");
+		MotBienParenthese.enumMotsBP(0);
+		MotBienParenthese.reset();
+		System.out.println("Les mots bien parenthésé de longueur 1 :");
+		MotBienParenthese.enumMotsBP(1);
+		MotBienParenthese.reset();
+		System.out.println("Les mots bien parenthésé de longueur 2 :");
+		MotBienParenthese.enumMotsBP(2);
+		System.out.println("Les mots bien parenthésé de longueur 3 :");
+		MotBienParenthese.enumMotsBP(3);
+		System.out.println("Les mots bien parenthésé de longueur 4 :");
+		MotBienParenthese.enumMotsBP(4);
 	}
 }
