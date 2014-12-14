6d953d2eef163f20b79e8ddd425aa37a8bcb6fd7
unknown <Arnaud@BXXX.polytech.unice.fr>
2014-12-14 11:40:06 +0100

    Ajout de la fonction récursive catalan(int n).

--- model/MotBienParenthese.java
+++ model/MotBienParenthese.java
@@ -15,83 +15,91 @@ public class MotBienParenthese {
 	private static int nb = 0;
 	private static int fin = 0;
 
-	public static void enumMotsBP(int n){	
-		n*=2;
+	public static void enumMotsBP(int n) {
+		n *= 2;
 
-		//Si 0 mot
-		if(n <= 0){ 
+		// Si 0 mot
+		if (n <= 0) {
 			System.out.println("0 mot");
 			return;
 		}
-		//Si 1 mot
-		else if(n == 2){ 
+		// Si 1 mot
+		else if (n == 2) {
 			addOuvrante();
 			addFermante();
 			System.out.println("");
 			return;
 		}
-		//Tous les autres
+		// Tous les autres
 		else {
-			//Test pour ajouter qu'au début le mot vide
-			if(nb == 0) list.add("");
-			
-			for(int j = 0; j < list.size() ; j++){
-				String a = apliquerConstructeur((String)list.get(nb),(String) list.get(j));
-				String b = apliquerConstructeur((String)list.get(j),(String) list.get(nb));
+			// Test pour ajouter qu'au début le mot vide
+			if (nb == 0)
+				list.add("");
 
-				if(a.length() <= n && !list.contains(a) && a != "")	list.add(a);
-				if (b.length() <= n && !list.contains(b) && b != "") list.add(b);
+			for (int j = 0; j < list.size(); j++) {
+				String a = apliquerConstructeur((String) list.get(nb),
+						(String) list.get(j));
+				String b = apliquerConstructeur((String) list.get(j),
+						(String) list.get(nb));
+
+				if (a.length() <= n && !list.contains(a) && a != "")
+					list.add(a);
+				if (b.length() <= n && !list.contains(b) && b != "")
+					list.add(b);
 			}
-			
-			//Si on a pas fini, on repart
-			if(nb < list.size() && ++nb < list.size() - 1 )	enumMotsBP(n/2);
-			
-			//Pour que la récursivité n'affiche pas n fois les () on condanme avec une constante au premier passage
-			if(fin == 0){
-				//On codanme
+
+			// Si on a pas fini, on repart
+			if (nb < list.size() && ++nb < list.size() - 1)
+				enumMotsBP(n / 2);
+
+			// Pour que la récursivité n'affiche pas n fois les () on condanme
+			// avec une constante au premier passage
+			if (fin == 0) {
+				// On codanme
 				fin++;
-				//Suppression des mots avec la mauvaise longueur et des doublons
-				delete(n);	deleteDoublons();
+				// Suppression des mots avec la mauvaise longueur et des
+				// doublons
+				delete(n);
+				deleteDoublons();
 				int i = 0;
-				//Affichage des mots BP
-				for( ; i < list.size();i++)	System.out.println(list.get(i));
-				
-				System.out.println("Soit "+i+" nombres");
+				// Affichage des mots BP
+				for (; i < list.size(); i++)
+					System.out.println(list.get(i));
+
+				System.out.println("Soit " + i + " nombres");
 			}
 		}
 	}
 
-
-		   
-	private static void delete(int n){
-		for(int i = 0 ; i < list.size(); i++)
-			if(((String) list.get(i)).length() != n ){
+	private static void delete(int n) {
+		for (int i = 0; i < list.size(); i++)
+			if (((String) list.get(i)).length() != n) {
 				list.remove(i);
 				i--;
 			}
-		
+
 	}
 
-	private static void deleteDoublons(){
-		Set set = new HashSet() ;
-		set.addAll(list) ;
-		ArrayList dis = new ArrayList(set) ;
+	private static void deleteDoublons() {
+		Set set = new HashSet();
+		set.addAll(list);
+		ArrayList dis = new ArrayList(set);
 		list = null;
 		list = dis;
 	}
 
-	private static String apliquerConstructeur(String a, String b){
-		return parentheseOuvrante + a + parentheseFermante +b;		
+	private static String apliquerConstructeur(String a, String b) {
+		return parentheseOuvrante + a + parentheseFermante + b;
 	}
 
-	private static void addOuvrante(){
+	private static void addOuvrante() {
 		System.out.print("(");
 		compteurOuvrante++;
 		compteur++;
 		altitude++;
 	}
 
-	public static void reset(){
+	public static void reset() {
 		nb = 0;
 		list = null;
 		altitude = 0;
@@ -101,13 +109,23 @@ public class MotBienParenthese {
 		compteurOuvrante = 0;
 		fin = 0;
 	}
-	private static void addFermante(){
+
+	private static void addFermante() {
 		System.out.print(")");
 		compteurFermante++;
 		compteur++;
 		altitude--;
 	}
 
-
+	public static int catalan(int n) {
+		if (n <= 1) {
+			return 1;
+		}
+		int res = 0;
+		for (int i = 0; i < n; i++) {
+			res += catalan(i) * catalan(n - i - 1);
+		}
+		return res;
+	}
 
 }
--- tests/TestMotBienParenthese.java
+++ tests/TestMotBienParenthese.java
@@ -11,21 +11,23 @@ public class TestMotBienParenthese {
 		System.out.print("Jusqu'à ? ");
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		
-		for(int i = 0 ; i <= a ; i++){
+
+		for (int i = 0; i <= a; i++) {
 
 			Date dStartDate = new Date();
 
-			System.out.println("Les mots bien parenthésé de longueur "+i+" :");
+			System.out.println("Les mots bien parenthésé de longueur " + i
+					+ " :");
 			MotBienParenthese.enumMotsBP(i);
 			MotBienParenthese.reset();
-			
+
 			Date dEndDate = new Date();
 			long lExecTime = dEndDate.getTime() - dStartDate.getTime();
-			System.out.println("Temps d'exécution pour "+i+" en millisecondes: " +  lExecTime );
-			
+			System.out.println("Temps d'exécution pour " + i
+					+ " en millisecondes: " + lExecTime);
+			System.out.println("Il y a " + MotBienParenthese.catalan(i)
+					+ " mot(s) de longueur " + i + ".");
 		}
-
 
 	}
 }
